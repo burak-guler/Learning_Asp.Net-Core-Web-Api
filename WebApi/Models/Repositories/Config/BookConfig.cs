729dbec3d1e265d61e6d89bfc9c897bae54b4382
burak-guler <burakguler258@gmail.com>
2024-03-10 15:43:14 +0300

    Add project files.

---
+++ WebApi/Models/Repositories/Config/BookConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace WebApi.Models.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            //migration aldığında eğer bu tabloda veri yoksa aşağıdaki işlemler yapılır.
+            //yani veri eklenir.
+            builder.HasData(
+                new Book {Id=1, Title = "Karagöz ve Hacivat", Price = 75 },
+                new Book { Id = 2, Title = "Mesnevi", Price = 175 },
+                new Book { Id = 3, Title = "Devlet", Price = 375 }
+                );
+        }
+    }
+}
---
+++ WebApi/Models/Repositories/RepositoryContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models.Repositories.Config;
+
+namespace WebApi.Models.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //model oluşturulurken BookConfig de yaptığımız configuration ifadeleri dikkate alınacak
+            modelBuilder.ApplyConfiguration(new BookConfig());
+        }
+    }
+}
